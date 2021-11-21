6462d1f447b46a896ad20a302d858996ce7b404d
gultiaeva <angultiaev@gmail.com>
2021-11-21 17:40:50 +0300

    [RF] Minor fix

--- common/vocabulary.py
+++ common/vocabulary.py
@@ -44,16 +44,3 @@ def load_vocab(path_to_vocab_dir: str) -> Vocabulary:
     """
     vocabulary = Vocabulary.from_files(path_to_vocab_dir)
     return vocabulary
-
-
-if __name__ == '__main__':
-    train_path = 'data/dataset/train_data.conllu.gz'
-    test_path = 'data/dataset/test_data.conllu.gz'
-    validation_path = 'data/dataset/validation_data.conllu.gz'
-    datasets_paths = [train_path, test_path, validation_path]
-
-    vocab_dir = 'data/vocab'
-
-    build_vocab(*datasets_paths, save_dir_path=vocab_dir)
-
-    vocab = load_vocab(vocab_dir)
